27ccf9d039b77db81d9a1f076cb97e8ac6dddbd8
Michael Etiennette <ME734@live.mdx.ac.uk>
2026-04-29 00:03:06 +0400

    corrected vite.config.js

--- dysf-agenda/client/vite.config.js
+++ dysf-agenda/client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/App/",
+  base: "/dysf-agenda/",
   plugins: [react()],
 });
