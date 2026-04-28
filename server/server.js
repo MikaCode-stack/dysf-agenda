e76b400418803777184d0427fcfa6793a530cd0e
Michael Etiennette <ME734@live.mdx.ac.uk>
2026-04-29 00:42:51 +0400

    changed everything

--- server/server.js
+++ server/server.js
@@ -6,7 +6,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://mikacode-stack.github.io"
+  ],
+  methods: ["GET", "POST", "PUT"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 // Serve built frontend in production
